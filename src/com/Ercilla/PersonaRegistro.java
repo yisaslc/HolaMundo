8932bd132c07b890cf2af0a218a184cc0afe95e0
Alejandro <jesusalvsuleiman@gmail.com>
2025-09-19 14:14:16 +0200

    Se agrego algo inservible

--- src/com/Ercilla/PersonaRegistro.java
+++ src/com/Ercilla/PersonaRegistro.java
@@ -2,6 +2,7 @@ package com.Ercilla;
 
 public record PersonaRegistro() {
     public String toString (){
+        System.out.println("Esta instruccion es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
-} s
+}
