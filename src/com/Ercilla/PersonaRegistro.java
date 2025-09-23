9817bdb860f10009c9d6ae3cd5f3ac61ddeabf38
Alejandro <jesusalvsuleiman@gmail.com>
2025-09-23 11:54:50 +0200

    Comentario

--- src/com/Ercilla/PersonaRegistro.java
+++ src/com/Ercilla/PersonaRegistro.java
@@ -2,7 +2,8 @@ package com.Ercilla;
 
 public record PersonaRegistro() {
     public String toString (){
-        System.out.println("Esta instruccion es inservible");
+        //La línea se puede eliminar
+        //System.out.println("Esta instruccion es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 }
